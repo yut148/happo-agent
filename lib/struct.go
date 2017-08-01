375505aa057be5accd78fab9af68a15a860dab2b
Toshiaki Baba <toshiaki@netmark.jp>
2017-08-01 13:05:10 +0900

    fix to pass golint(exclude don't use ALL_CAPS in Go names)

--- lib/struct.go
+++ lib/struct.go
@@ -2,13 +2,14 @@ package lib
 
 // --- Struct
 
-// metric buffer
+// MetricsData is actual metrics
 type MetricsData struct {
 	HostName  string             `json:"hostname"`
 	Timestamp int64              `json:"timestamp"`
 	Metrics   map[string]float64 `json:"metrics"`
 }
 
+// InventoryData is actual inventory
 type InventoryData struct {
 	GroupName     string `json:"group_name"`
 	IP            string `json:"ip"`
@@ -21,45 +22,45 @@ type InventoryData struct {
 
 // --- Request Parameter
 
-// /proxy
+// ProxyRequest is /proxy API
 type ProxyRequest struct {
 	ProxyHostPort []string `json:"proxy_hostport"`
 	RequestType   string   `json:"request_type"`
 	RequestJSON   []byte   `json:"request_json"`
 }
 
-// /monitor API
+// MonitorRequest is /monitor API
 type MonitorRequest struct {
 	APIKey       string `json:"apikey"`
 	PluginName   string `json:"plugin_name"  binding:"required"`
 	PluginOption string `json:"plugin_option"`
 }
 
-// /metric API
+// MetricRequest is /metric API
 type MetricRequest struct {
 	APIKey string `json:"apikey"`
 }
 
-// /metric/append API
+// MetricAppendRequest is /metric/append API
 type MetricAppendRequest struct {
 	APIKey     string        `json:"apikey"`
 	MetricData []MetricsData `json:"metric_data"`
 }
 
-// /metric/config/update API
+// MetricConfigUpdateRequest is /metric/config/update API
 type MetricConfigUpdateRequest struct {
 	APIKey string       `json:"apikey"`
 	Config MetricConfig `json:"config"`
 }
 
-// /inventory API
+// InventoryRequest is /inventory API
 type InventoryRequest struct {
 	APIKey        string `json:"apikey"`
 	Command       string `json:"command"`
 	CommandOption string `json:"command_option"`
 }
 
-// Manage API
+// ManageRequest is Manage API
 type ManageRequest struct {
 	APIKey   string           `json:"apikey"`
 	Hostdata CrawlConfigAgent `json:"hostdata"`
@@ -67,37 +68,37 @@ type ManageRequest struct {
 
 // --- Response Parameter
 
-// /monitor API
+// MonitorResponse is /monitor API
 type MonitorResponse struct {
 	ReturnValue int    `json:"return_value"`
 	Message     string `json:"message"`
 }
 
-// /metric API
+// MetricResponse is /metric API
 type MetricResponse struct {
 	MetricData []MetricsData `json:"metric_data"`
 	Message    string        `json:"message"`
 }
 
-// /metric/append API
+// MetricAppendResponse is /metric/append API
 type MetricAppendResponse struct {
 	Status  string `json:"status"`
 	Message string `json:"message"`
 }
 
-// /metric/config/update API
+// MetricConfigUpdateResponse is /metric/config/update API
 type MetricConfigUpdateResponse struct {
 	Status  string `json:"status"`
 	Message string `json:"message"`
 }
 
-// /inventory API
+// InventoryResponse is /inventory API
 type InventoryResponse struct {
 	ReturnCode  int    `json:"return_code"`
 	ReturnValue string `json:"return_value"`
 }
 
-// Manage API
+// ManageResponse is Manage API
 type ManageResponse struct {
 	Status  string `json:"status"`
 	Message string `json:"message"`
