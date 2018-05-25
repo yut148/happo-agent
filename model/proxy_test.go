13fa71dc057993d360a41063c97827fd818747b4
shiimaxx <yoshima@shiimaxx.com>
2018-05-25 14:51:55 +0900

    Add test for proxy monitor request to autoscaling instance

--- model/proxy_test.go
+++ model/proxy_test.go
@@ -11,12 +11,44 @@ import (
 	"testing"
 	"time"
 
+	"os"
+
 	"github.com/codegangsta/martini-contrib/render"
 	"github.com/go-martini/martini"
+	"github.com/heartbeatsjp/happo-agent/autoscaling"
+	"github.com/heartbeatsjp/happo-agent/autoscaling/awsmock"
+	"github.com/heartbeatsjp/happo-agent/db"
 	"github.com/heartbeatsjp/happo-agent/halib"
 	"github.com/martini-contrib/binding"
 	"github.com/stretchr/testify/assert"
+	"github.com/syndtr/goleveldb/leveldb"
+	"github.com/syndtr/goleveldb/leveldb/storage"
+	leveldbUtil "github.com/syndtr/goleveldb/leveldb/util"
 )
+
+func setup() {
+	//Mock
+	DB, err := leveldb.Open(storage.NewMemStorage(), nil)
+	if err != nil {
+		os.Exit(1)
+	}
+	db.DB = DB
+}
+
+func teardown() {
+	iter := db.DB.NewIterator(
+		leveldbUtil.BytesPrefix(
+			[]byte("ag-"),
+		),
+		nil,
+	)
+	for iter.Next() {
+		key := iter.Key()
+		db.DB.Delete(key, nil)
+	}
+	iter.Release()
+	db.DB.Close()
+}
 
 func TestPostToAgent1(t *testing.T) {
 	const stubResponse = "OK"
@@ -290,6 +322,56 @@ func TestProxy4(t *testing.T) {
 	assert.Equal(t, http.StatusGatewayTimeout, res.Code)
 	assert.Equal(t,
 		fmt.Sprintf(`{"return_value":3,"message":"Post https://%s:%d/proxy: net/http: request canceled while waiting for connection (Client.Timeout exceeded while awaiting headers)"}`, host, port),
+		res.Body.String(),
+	)
+}
+
+func TestProxy5(t *testing.T) {
+	//dispatch autoscaling instance
+
+	setup()
+	client := &autoscaling.AWSClient{
+		SvcEC2:         &awsmock.MockEC2Client{},
+		SvcAutoscaling: &awsmock.MockAutoScalingClient{},
+	}
+	autoscaling.RefreshAutoScalingInstances(client, "dummy-prod-ag", "dummy-prod-app", 10)
+	defer teardown()
+
+	//bastion
+	m := martini.Classic()
+	m.Use(render.Renderer())
+	m.Post("/proxy", binding.Json(halib.ProxyRequest{}), Proxy)
+
+	//edge
+	ts := httptest.NewTLSServer(
+		http.HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				fmt.Fprint(w, `{"return_value":0,"message":"ok"}`)
+			}))
+	ts.URL = "http://192.0.2.11:6777"
+	defer ts.Close()
+
+	alias := "dummy-prod-ag-dummy-prod-app-1"
+	port := 6777
+
+	requestJSON := fmt.Sprintf(`{
+		"proxy_hostport": ["%s:%d"],
+		"request_type": "monitor",
+		"request_json":
+			"{\"apikey\": \"\", \"plugin_name\": \"monitor_test_plugin\", \"plugin_option\": \"0\"}"
+	}`, alias, port)
+	reader := bytes.NewReader([]byte(requestJSON))
+
+	req, _ := http.NewRequest("POST", "/proxy", reader)
+	req.Header.Set("Content-Type", "application/json")
+
+	res := httptest.NewRecorder()
+
+	m.ServeHTTP(res, req)
+
+	assert.Equal(t, http.StatusOK, res.Code)
+	assert.Equal(t,
+		`{"return_value":0,"message":"ok"}`,
 		res.Body.String(),
 	)
 }
